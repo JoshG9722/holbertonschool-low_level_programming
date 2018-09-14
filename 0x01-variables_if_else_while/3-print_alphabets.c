3f28c0e1fcc44fb7eb8c99958e9025f9a87122cb
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-13 18:00:34 -0700

    Updating format

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 /**
-    * main - entry point
-    * Return: always 0 (success)
-    **/
-    int main(void)
-    {
-        char al;
-        char Upal;
+  * main - entry point
+  * Return: always 0(success)
+**/
+int main(void)
+{
+	char al, Upal;
 
-        for (al = 'a'; al <= 'z'; al++)
-        {
-                putchar(al);
-        }
-
-        for (Upal = 'A'; Upal <= 'Z'; Upal++)
-        {
-                putchar(Upal);
-        }
-        putchar('\n');
-        return(0);
-    }
+	for (al = 'a'; al <= 'z'; al++)
+		putchar(al);
+		for (Upal = 'A'; Upal <= 'Z'; Upal++)
+			putchar(Upal);
+	putchar('\n');
+	return (0);
+}
