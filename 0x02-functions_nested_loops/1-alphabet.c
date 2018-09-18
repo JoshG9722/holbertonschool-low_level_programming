649c4407c0fdeab6af8447ba6ee105cbac1c1e39
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-18 13:53:00 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
  * print_alphabet - entry point
- * Return: always 0 (success)
-**/
-
+ * Return: Always 0 (success)
+ */
 void print_alphabet(void)
 {
-    int al;
+	int al;
 
 	for (al = 'a'; al <= 'z'; al++)
+	{
 		_putchar(al);
+	}
 	_putchar('\n');
 }
