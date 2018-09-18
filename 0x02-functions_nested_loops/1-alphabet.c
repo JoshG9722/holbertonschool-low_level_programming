f1b2107df0323d9a0b5126ef0ee4a9a68ac0b696
JoshG9722 <joshgonzalez092297@gmail.com>
2018-09-18 12:24:35 -0700

    Prints alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ * Return: always 0 (success)
+**/
+
+int main(void)
+{
+    int al;
+
+	for (al = 'a'; al <= 'z'; al++)
+		_putchar(al);
+	_putchar('\n');
+	return (0);
+}
