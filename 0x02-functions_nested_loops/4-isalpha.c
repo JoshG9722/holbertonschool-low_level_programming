7e3ffc3abbec3b8e97e4a84f4e4dedfea5806511
JoshG9722 <joshgonzalez092297@gmail.com>
2018-09-18 16:52:58 -0700

    returns 1 if upper or lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * islower - checks if letter is lowercase
+ * @c: checks to see if char is lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        return (1);
+    }
+    else if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+    {
+        return(0);
+    }
+}
