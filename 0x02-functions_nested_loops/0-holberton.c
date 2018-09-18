d35b440cc7cbfd8b732c929c49f68ab2e47ac53d
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-18 12:06:00 -0700

    Including the header file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - entry point
  * Return: always 0 (success)
- */
+**/
 int main(void)
 {
-	puts("Holberton \n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
