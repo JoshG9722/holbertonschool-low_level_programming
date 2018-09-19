a7292e383b79ee0f4e52fa3461be89104d202ef5
JoshG9722 <joshgonzalez092297@gmail.com>
2018-09-18 22:57:02 -0700

    counts all time in a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * jack_bauer - entry point
+ * @void 
+ * Return:void
+ */
+void jack_bauer(void)
+{
+    int i;
+    int n;
+
+    for (i = 0; i <= 23; i++)
+    {
+            for (n = 0; n <= 59; n++)
+            {
+                    _putchar(i / 10 + '0');
+                    _putchar(i % 10 + '0');
+                    _putchar(':');
+                    _putchar(n / 10 + '0');
+                    _putchar(n % 10 + '0');
+                    _putchar('\n');
+            }
+    }
+
+}
+
