537a4273a1ecf65f344952217df8cc05986a4181
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-18 23:02:25 -0700

    updated format

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,24 @@
 #include "holberton.h"
 /**
  * jack_bauer - entry point
- * @void 
- * Return:void
+ * @void
+ * return - void
  */
 void jack_bauer(void)
 {
-    int i;
-    int n;
+	int i;
+	int n;
 
-    for (i = 0; i <= 23; i++)
-    {
-            for (n = 0; n <= 59; n++)
-            {
-                    _putchar(i / 10 + '0');
-                    _putchar(i % 10 + '0');
-                    _putchar(':');
-                    _putchar(n / 10 + '0');
-                    _putchar(n % 10 + '0');
-                    _putchar('\n');
-            }
-    }
-
+	for (i = 0; i <= 23; i++)
+	{
+		for (n = 0; n <= 59; n++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
-
