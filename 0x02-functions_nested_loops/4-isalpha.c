b7d289510f84543550dc5c93477915c9810a54d7
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-18 17:06:34 -0700

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,19 @@
 #include "holberton.h"
 /**
- * isalpha - checks if letter is lowercase or uppercase
- * @c: checks to see if char is lowercase or uppercase
- * Return: Always 0 (success)
+ * _isalpha - entry point
+ * @c: checks if char is upper or lowercase
+ * Return: 0 or 1 depending on c
  */
 int _isalpha(int c)
 {
-    if (c >= 'a' && c <= 'z')
-    {
-        return (1);
-    }
-    else if (c >= 'A' && c <= 'Z')
-    {
-        return (1);
-    }
-    else
-    {
-        return(0);
-    }
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
