b57f80822638010183086ac23355f52390bfac37
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-20 18:27:57 -0700

    Update 3-print_numbers.c

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
  * print_numbers - prints nums 0 to 9
+ * void - Empty argument
  */
 void print_numbers(void)
 {
 	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar('\n');
 }
