e84208e9334e5fee57dde9bf34b6e34b29483b26
JoshG9722 <joshgonzalez092297@gmail.com>
2018-09-20 18:10:31 -0700

    Prints 0-9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - print nums 0-9
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
