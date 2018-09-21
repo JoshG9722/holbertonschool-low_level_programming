243a3e28bbac381c714bf3319c05112beff7a692
JoshG9722 <joshgonzalez092297@gmail.com>
2018-09-20 17:54:13 -0700

    Multiplioes two ints

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * mult - entry point
+ * @a: num passed to function
+ * @b: num passed to function
+ * Return: value of c
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
+
+
