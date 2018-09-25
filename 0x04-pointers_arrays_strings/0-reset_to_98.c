eb9babb7bcb38621e75e375a5b9f4eba6c9b75d4
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-25 11:39:09 -0700

    updated comments

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-  * reset_to_98 - updates value to 98
-  * return: void
-  */
+ * reset_to_98 - updates value to 98
+ * @n: pointer passed to function
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
