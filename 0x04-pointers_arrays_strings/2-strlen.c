3d09fb9b540f3e3c367c7a097709276b763422c1
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-25 11:49:31 -0700

    Updated comments

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
-  * _strlen_ - returns length of string
-  * return - i
-  */
+ * _strlen - returns length of string
+ * @s: takes a string
+ * Return: i
+ */
 int _strlen(char *s)
 {
 	int i = 0;
