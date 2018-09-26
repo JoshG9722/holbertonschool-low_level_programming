be04a0c305467aef025800e3a35f9795b39ec800
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-09-25 17:57:04 -0700

    updated format

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 /**
-  * puts_half - prints half of string
-  * @s: takes string
-  */
-void puts_half (char *str)
+ * puts_half - prints half of string
+ * @str: takes string
+ */
+void puts_half(char *str)
 {
-    int i, hlf, lngth;
-    
-    for (i = 0; str[i] != '\0'; i++)
-            lngth++;
-            
-    if (!(lngth % 2))
-            hlf = lngth / 2;
-    else
-            hlf = (lngth + 1) / 2;
-    for (i = hlf; str[i] != '\0'; i++)
-            _putchar(str[i]);
-    _putchar('\n');
+	int i, hlf, lngth;
+
+	lngth = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		lngth++;
+
+	if (!(lngth % 2))
+		hlf = lngth / 2;
+	else
+		hlf = (lngth + 1) / 2;
+
+	for (i = hlf; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
