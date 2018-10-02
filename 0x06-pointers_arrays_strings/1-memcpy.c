050ba547f2df103663079ffd87cce584ee710669
JoshG9722 <joshgonzalez092297@gmail.com>
2018-10-01 17:59:14 -0700

    copies memory area

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * memcpy - copies memory of src to dest
+  * @dest: destination
+  * @src: source
+  * @n: buffer size
+  * Return: dest
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
