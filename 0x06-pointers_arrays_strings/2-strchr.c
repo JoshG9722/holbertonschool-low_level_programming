66130e0311f3b26c529ca36745453db06ba2a27a
JoshG9722 <joshgonzalez092297@gmail.com>
2018-10-01 18:04:49 -0700

    locates character in string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  * _strchr - locates character in string
+  * @s: string
+  * @c: character
+  * Return: s + i
+  */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			break;
+	}
+	if (s[i] == c)
+		return (s + i);
+	return ('\0');
+}
