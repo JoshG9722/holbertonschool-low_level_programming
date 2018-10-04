dfae133a74abbe05092793e1aee010fc7fa45a26
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-10-03 20:28:19 -0700

    Checks if string is palindrome

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,49 +1,44 @@
-#include "holberton.h"
+int str_length(char *s);
+int _check(char *s, int i, int j);
 /**
-  * is_palindrome - checks on palindrome
+  * is_palindrome - checks if string is palindrome
   * @s: takes string
   * Return: int
   */
 int is_palindrome(char *s)
 {
-	int length;
-
-	length = _strlen(s);
-
-	if (length <= 1)
-		return (1);
-	return (_check(s, length));
-}
-/**
-  * _strlen - string length
-  * @s: takes string
-  * Return: int
-  */
-int _strlen(char *s)
-{
+	int size;
 
 	if (*s == '\0')
-		return (0);
-	return (1 + _strlen(s + 1));
+		return (1);
+	size = str_length(s);
+	return (_check(s, 0, size - 1));
 }
+
 /**
-  * _check - checks if palindrome
+  * _check - checks string
   * @s: takes string
-  * @l: length
+  * @i: int
+  * @j: int
   * Return: int
   */
-int _check(char *s, int l)
+int _check(char *s, int i, int j)
 {
-	if (l <= 1)
-	{
+	if (i > j / 2)
 		return (1);
-	}
-	if (*s != s[l - 1])
-	{
-		return (0);
-	}
+	if (*(s + i) == *(s + j - i))
+		return (_check(s, i + 1, j));
 	else
-	{
-		return (_check(s + 1, l - 2));
-	}
+		return (0);
+}
+/**
+  * str_length - returns length of string
+  * @s: takes string
+  * Return: int
+  */
+int str_length(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + str_length(s + 1));
 }
