36bc0c0713a86626ef7b4842ab5ba9715f4ddd82
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-10-03 17:34:10 -0700

    prints string in reverse

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,13 +1,18 @@
 #include "holberton.h"
 /**
-  * _print_rev_recursion - prints string in reverse 
-  * @s: takes string
-  */
+ * _print_rev_recursion - prints string in reverse
+ * @s: takes string
+ * Return: void
+ */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
+		return;
+	}
+	else
+	{
+		_print_rev_recursion((char *)s + 1);
 		_putchar(*s);
 	}
 }
