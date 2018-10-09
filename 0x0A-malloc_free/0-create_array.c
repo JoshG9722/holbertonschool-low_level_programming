80c74cb2088f783b9ab24ad3a0f2e323ea0f707d
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-10-09 14:51:37 -0700

    creates array of chars

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,25 +1,25 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * create_array - creates an array of chars, initializes with speficic char
- * @size: size of the array
- * @c: starting character
- * return: ar_ptr
- */
+  * create_array - creates array of chars, initialized with specific char
+  * @size: size of array
+  * @c: starting char
+  * Return: ar_ptr
+  */
 char *create_array(unsigned int size, char c)
 {
-  char *ar_ptr;
-  unsigned int i;
+	char *ar_ptr;
+	unsigned int i;
 
-  if (size == 0)
-    {
-      return (NULL);
-    }
-  else
-    {
-      ar_ptr = malloc(size * sizeof(c));
-    }
-  for(i = 0; i < size; i++)
-    *(ar_ptr + i) = c;
-  return(ar_ptr);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ar_ptr = malloc(size * sizeof(c));
+	}
+	for (i = 0; i < size; i++)
+		*(ar_ptr + i) = c;
+	return (ar_ptr);
 }
