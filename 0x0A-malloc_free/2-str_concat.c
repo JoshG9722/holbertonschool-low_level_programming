bcac81f273040bcd50de3e6a6f9d820dd421f681
JoshG9722 <40642252+JoshG9722@users.noreply.github.com>
2018-10-09 18:58:06 -0700

    concats two strings

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 	int k;
-	char *r_ptr;
+	char *ret_ptr;
 
 	i = 0;
 	j = 0;
@@ -24,18 +24,22 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (!s2)
 		s2 = "";
-	while(s2[j])
-		j = j + 1;
-	r_ptr = malloc((i + j) * sizeof(char) + 1);
-	if (r_ptr == NULL);
+	while (s1[i])
+		i += 1;
+	while (s2[j])
+		j += 1;
+	ret_ptr = malloc((i + j) * sizeof(char) + 1);
+	if (ret_ptr == NULL)
+		return (NULL);
 	while (*s1)
 	{
-		r_ptr[k] = *s1++;
-		k = k + 1;
+		ret_ptr[k] = *s1++;
+		k += 1;
 	}
 	while (*s2)
 	{
-		r_ptr[k] = *s2++;
+		ret_ptr[k] = *s2++;
+		k += 1;
 	}
-	return (r_ptr);
+	return (ret_ptr);
 }
